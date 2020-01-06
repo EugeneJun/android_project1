dc39e2c5d098d6415dfd32444038fcaa18dd4404
SuYoung Chae <tndudaksen12@gmail.com>
2020-01-06 22:10:02 +0900

    add new folder of Suyoung's work

---
+++ RegisterLogin/app/src/main/java/com/example/registerlogin/RegisterRequest.java
@@ -0,0 +1,99 @@
+package com.example.registerlogin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterRequest extends AppCompatActivity {
+
+    private RequestQueue requestQuere;
+    private EditText et_id, et_pass;
+    private Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_id = (EditText)findViewById(R.id.et_id);
+        et_pass = (EditText)findViewById(R.id.et_pass);
+
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = "{"+
+                        "\"id\"" + "\"" + et_id.getText().toString() + "\","+
+                        "\"pass\"" + "\"" + et_pass.getText().toString() + "\""+
+                        "}";
+                Submit(data);
+            }
+        });
+    }
+
+    private void Submit(String data)
+    {
+        final String savedata= data;
+        String URL="https://106.10.59.45:8080";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject objres=new JSONObject(response);
+                    Toast.makeText(getApplicationContext(),objres.toString(),Toast.LENGTH_LONG).show();
+
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
+
+                }
+                //Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                //Log.v("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return savedata == null ? null : savedata.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    //Log.v("Unsupported Encoding while trying to get the bytes", data);
+                    return null;
+                }
+            }
+
+        };
+        requestQueue.add(stringRequest);
+    }
+}
